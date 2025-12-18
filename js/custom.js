6006b8b3cbaeb88cad417eb9785f2fb075993c7d
nirbhayrana123 <nirbhydesign@gmail.com>
2025-12-18 18:19:26 +0530

    abhi

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,10 @@
-  const editBtn = document.querySelector('.menu');
-    const model = document.querySelector('.header');
+const editBtn = document.querySelector('.menu');
+const model = document.querySelector('.header');
 
-    editBtn.addEventListener('click', function () {
-      model.classList.toggle('active');
-    });
+editBtn.addEventListener('click', function () {
+  model.classList.toggle('active');
+});
+
+
+
+ 
