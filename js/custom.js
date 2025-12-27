254ba1c1ab28a0d3a4a563a120f57804bf4dde41
nirbhayrana123 <nirbhydesign@gmail.com>
2025-12-27 17:26:55 +0530

    commit in css slider

--- js/custom.js
+++ js/custom.js
@@ -144,6 +144,24 @@ jQuery(function ($) {
 
 
 
+jQuery(function ($) { 
+  $('.services-grid').owlCarousel({
+    loop: true,
+    margin: 20,
+    nav: true,
+    
+    responsive: {
+      0: { items: 1 },
+      600: { items: 2 },
+      1000: { items:4 }
+    }
+  });
+
+});
+
+
+
+
 
 
 
